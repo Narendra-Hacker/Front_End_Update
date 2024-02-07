226a6c77766ab166f7f26f1c87c2c7a15ca25d4e
Narendra-Hacker <gnarendrakumar8@gmail.com>
2024-02-07 12:54:30 +0000

    new chnages

--- src/components/sample.js
+++ src/components/sample.js
@@ -8,7 +8,7 @@ import { useNavigate } from "react-router-dom";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 
-const Receipt = () => {
+const Receipt = () => { 
   // const navigate = useNavigate();
 
   const params = useParams();
